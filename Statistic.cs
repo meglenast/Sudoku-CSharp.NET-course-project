96b2c0202b33e45eac2c7ab06b2c617f14d950dd
meglenast <58610466+meglenast@users.noreply.github.com>
2021-02-11 21:02:48 +0200

    statistic added

---
+++ Statistic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    
+    class Statistic
+    {
+        private static readonly string statFileName = "stat.txt";
+
+        /// <summary>
+        /// Method that adds a game to statistic
+        /// </summary>
+        /// <param name="difficulty"></param>
+        /// <param name="isWin"></param>
+        public static void AddToStatistic(Difficulty difficulty, bool isWin)
+        {
+            if (File.Exists(statFileName))
+            {
+                string[] data = File.ReadAllLines(statFileName);
+
+                int numGamesPlayed = Convert.ToInt32(data[0]);
+                ++numGamesPlayed;
+                int numGamesWon = Convert.ToInt32(data[1]);
+                if (isWin) ++numGamesWon;
+
+                data[0] = numGamesPlayed.ToString();
+                data[1] = numGamesWon.ToString();
+                data[(int)difficulty] = (Convert.ToInt32(data[(int)difficulty]) + 1).ToString();
+
+                File.WriteAllLines(statFileName, data);
+            }
+            else
+            {
+                string[] record = { "1", "0", "0", "0", "0"};
+                if (isWin)
+                {
+                    record[1] = "1";
+                    record[(int)difficulty] = "1";
+                }
+                else
+                {
+                    record[(int)difficulty] = "1";
+                }
+                File.WriteAllLines(statFileName, record);
+            }
+        }
+
+        /// <summary>
+        /// Method that reads games statisc 
+        /// </summary>
+        /// <returns></returns>
+        public static string[] GetStatInformation() 
+            => File.ReadAllLines(statFileName);
+    }
+}
---
+++ StatisticsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Interaction logic for StatisticsPage.xaml
+    /// </summary>
+    public partial class StatisticsPage : Page
+    {
+        public StatisticsPage()
+        {
+            InitializeComponent();
+
+            string[] statistic = Statistic.GetStatInformation();
+
+            // Initializing the WPF components with  information from file
+            TxtTotalNumGamesPlayed.Text = statistic[0];
+            TxtTotalNumGamesWon.Text = statistic[1];
+            TxtTotalNumGamesEASY.Text = statistic[2];
+            TxtTotalNumGamesMEDIUM.Text = statistic[3];
+            TxtTotalNumGamesHARD.Text = statistic[4];
+        }
+    }
+}
